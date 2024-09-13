f0426970d8259c5569783c9db25744a320844c9b
zincdev <zincdev0@gmail.com>
2024-09-13 11:17:58 -0700

    Tests for Ecs::{new, new_component}

--- crates/prophet_ecs/tests/ecs.rs
+++ crates/prophet_ecs/tests/ecs.rs
@@ -1,5 +1,18 @@
 #[cfg(test)]
 
+extern crate prophet_ecs;
+
+use prophet_ecs::Ecs;
+
 #[test]
-fn add_component() {
+fn new()
+        -> () {
+    let _ecs = Ecs::new();
+}
+
+#[test]
+fn new_component()
+        -> () {
+    let mut ecs = Ecs::new();
+    let _component = ecs.new_component();
 }
